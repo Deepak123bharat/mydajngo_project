2339fc91ad90ca68f15e9cc903fdc4cb4d2ba555
Deepak123bharat <33999326+Deepak123bharat@users.noreply.github.com>
2018-10-01 00:12:23 +0530

    Update urls.py

--- boards/urls.py
+++ boards/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+# this file contain url for boards
 urlpatterns = [
     path('boards/<int:pk>/',views.board_topics,name='board_topics'),
     path('boards/<int:pk>/new/',views.new_topic,name='new_topic'),
